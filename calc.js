24fbb3c538c6605f515efe289cbda0842ccfb830
Alok Maurya <alok.maurya@w3villa.com>
2023-07-21 12:02:21 +0530

    calculator completed javascript

--- calc.js
+++ calc.js
@@ -27,22 +27,7 @@ let calculator = document.getElementsByClassName('calculator-box');
 let keys = document.getElementById('calc');
 let a=document.getElementById('input-value').value;
 
-const calculate = (n1, operator, n2) => {
-    let result = ''
-    
-    if (operator =='addition') {
-      result = n1 + n2
-    } else if (operator === 'substraction') {
-      result = n1 - n2
-    } else if (operator === 'multiplication') {
-      result = n1 * n2
-    } else if (operator === 'divide') {
-      result = n1 / n2
-    }
-    
-    return result
-  }
-  const display = document.getElementById('input-value');
+  let display = document.getElementById('input-value');
 
 
    keys.addEventListener('click', e => {
@@ -50,111 +35,97 @@ const calculate = (n1, operator, n2) => {
     const action = key.dataset.action;
     const keycontent = key.textContent;
     const displaynum = display.value;
+
     
+    let previousKeyType;
     if(e.target.matches('button')){
         Array.from(key.parentNode.children)
         .forEach(k => k.classList.remove('operator-pressed'));
-        var operatorpressed=false;
-        var previousKeyType ;
-        if(!action){
-            
-            if(displaynum == '0' || previousKeyType == 'operator'){
-                display.value = keycontent;
-                
-                
-            }
-            else{
-                display.value=displaynum + keycontent;
-            }
-
-            console.log(display.value);
-            console.log(calculate(display.value.slice(-1),operator,display.value.slice(-3)));
- 
-        }
-        if(action=='decimal'){
-            display.value=displaynum + keycontent;
-            
-        }
-
         if(action=='addition' ||
             action=='substraction'||
             action=='multiply' ||
             action == 'divide' 
         
             )
-            
-            
             {
                 key.classList.add('operator-pressed');
                
-                display.value = displaynum+keycontent;
-                var operator = display.value.slice(-1);
-                 console.log(operator);
-                 if(operator=='+'){
-                    operator='addition';
-                 }
-                 if(operator=='-'){
-                    operator='substraction';
-                 }
-                 if(operator=='×'){
-                    operator='multiplication';
-                 }
-                 if(operator=='÷'){
-                    operator='divide';
-                 }
+                // display.value = displaynum+keycontent;
+                 previousKeyType='operator';
+                if(display.value.slice(-1)=='+' 
+                || display.value.slice(-1)=='-' 
+                || display.value.slice(-1)=='/' 
+                || display.value.slice(-1)=='*'){
+                    
+                   display.value= display.value.slice(0, -1) + keycontent;
+                    // console.log(display.value);
+                    // console.log(display.value.slice(-1)=keycontent) ;
+                }else{
+                    console.log('err');
 
-                  previousKeyType='operator';
-                  const firstval = Number(displaynum);
-                 console.log(firstval);
-
+                }
+                display.value = eval(display.value)+keycontent;
+ 
                 
+            }
         
+     
+        if(!action){
+            // if(document.getElementsByClassName('equal-pressed')){
+            //     display.value=0;
+            // }
+            
+            if(displaynum == '0' ){
+                display.value = keycontent;
+            }
+            else{
+                display.value=displaynum + keycontent;
+            }
 
-    }
+ 
+        }
+        if(action=='decimal'){
+            // if(document.getElementsByClassName('equal-pressed')){
+            //     display.value=0;
+            // }
+            
+            if(display.value.slice(-1) == '.'){
+                    
+                   display.value= display.value.slice(0, -1) + keycontent;
+            }
+            else{
+                display.value=displaynum + keycontent;
+            }
+            
+        }
+
+
     if(action=='equal'){
-        var a =Number(displaynum.slice(-1));
-        var secondvalue = a;
-        console.log(secondvalue);
+        // key.classList.add('equal-pressed');
+        if(display.value.slice(-1)=='+' 
+        || display.value.slice(-1)=='-' 
+        || display.value.slice(-1)=='/' 
+        || display.value.slice(-1)=='*'){
+            display.value='error';
+        }else{
+            display.value = eval(displaynum);
+        }
+
+        
+        
+        
     }
 
-        // var num = display.value;
-
-        // var op  = num.charAt(num.length - 2);
-        
-        // var n2=num.charAt(num.length - 1);
-        // const n1 = num.charAt(num.length - 3);
-        // if(op=='+'){
-        //     var operator = 'add';
-        // }
-
-        // const val = calculate(n1,operator,n2);
-        // console.log(val);
         
     }
     if(action=='ac'){
         display.value=0;
     }
     if(action=='cancel'){
-        display.value=0;
+        display.value=display.value.slice(0, -1);
     }
 
-    if(action=='calculate'){
-        
-    // console.log(display.value);
 
-
-}
-
-
-    
    })
 
 
-function calculate1(n1,operator,n2){
-    var result;
-    if(operator=='+'){
-        result = n1+n2;
-
-    }
-    return result;
-}
